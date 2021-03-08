4863d4b836aca24e3b6b78f9edbeefb1973d59c0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-08 19:41:17 +0100

    Version

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+try {
+    // Gets the version, if available
+    let version = core.getInput('version');
+    // TODO If the version is not provided, computes the latest
+    if (!version) {
+        console.log("No version provided. Getting the latest version from GitHub.")
+        version = '0.0.9';
+    }
+    console.log(`Using version: ${version}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
