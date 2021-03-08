26a18e6ff1b5e6e722d8b3550f56b0d9a0b22faf
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-08 20:03:19 +0100

    OS/Arch mapping

--- index.js
+++ index.js
@@ -13,10 +13,29 @@ try {
     console.log(`Using version: ${version}`);
 
     // Information about the OS
-    const osPlatform = os.platform();
-    const osArch = os.arch();
+    const osPlatform = mapOS(os.platform());
+    const osArch = mapArch(os.arch());
     console.log(`For OS platform: ${osPlatform}`);
     console.log(`For OS arch: ${osArch}`);
 } catch (error) {
     core.setFailed(error.message);
+}
+
+// arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
+// return value in [amd64, 386, arm]
+function mapArch (arch) {
+    const mappings = {
+        x32: '386',
+        x64: 'amd64'
+    };
+    return mappings[arch] || arch;
+}
+
+// os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
+// return value in [darwin, linux, windows]
+function mapOS (os) {
+    const mappings = {
+        win32: 'windows'
+    };
+    return mappings[os] || os;
 }
