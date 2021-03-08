c28e16dce64b451cff3d18ec50537c6de9f1dd52
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-08 22:19:31 +0100

    Restructuring + path setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const core = require('@actions/core');
+const os = require('os');
+const path = require('path');
 
-const setup = require('lib/setup-cli');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
 
 (async () => {
     try {
@@ -9,3 +12,62 @@ const setup = require('lib/setup-cli');
         core.setFailed(error.message);
     }
 })();
+
+async function setup() {
+    // Gets the version, if available
+    let version = core.getInput('version');
+    // TODO If the version is not provided, computes the latest
+    if (!version) {
+        console.log("No version provided. Getting the latest version from GitHub.")
+        version = '0.0.9';
+    }
+    console.log(`Using version: ${version}`);
+
+    // Information about the OS
+    const osPlatform = mapOS(os.platform());
+    const osArch = mapArch(os.arch());
+    console.log(`For OS platform: ${osPlatform}`);
+    console.log(`For OS arch: ${osArch}`);
+
+    // Getting the URL to the CLI
+    const url = `https://github.com/nemerosa/ontrack-cli/releases/download/${version}/ontrack-cli-${osPlatform}-${osArch}`;
+    console.log(`Downloading CLI from ${url}`);
+
+    // Downloading
+    await downloadAndSetup(url)
+}
+
+async function downloadAndSetup(url) {
+    const cliPath = await tc.downloadTool(url);
+    console.log(`Downloaded at ${cliPath}`)
+
+    const dir = path.dirname(cliPath)
+    console.log(`Directory is ${dir}`)
+
+    // If we're on Windows, then the executable ends with .exe
+    const exeSuffix = os.platform().startsWith('win') ? '.exe' : '';
+
+    await io.mv(cliPath,[dir, `ontrack-cli${exeSuffix}`].join(path.sep))
+
+    // Add to the path
+    core.addPath(dir)
+}
+
+// arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
+// return value in [amd64, 386, arm]
+function mapArch(arch) {
+    const mappings = {
+        x32: '386',
+        x64: 'amd64'
+    };
+    return mappings[arch] || arch;
+}
+
+// os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
+// return value in [darwin, linux, windows]
+function mapOS(os) {
+    const mappings = {
+        win32: 'windows'
+    };
+    return mappings[os] || os;
+}
