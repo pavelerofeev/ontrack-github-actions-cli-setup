4aa42009f811533acdfce3b0af6ad2d357165124
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-28 16:56:45 +0200

    Correction of tagging script

--- tag.sh
+++ tag.sh
@@ -8,15 +8,20 @@ PATCH_NAME=v${MAJOR}.${MINOR}.${PATCH}
 MINOR_NAME=v${MAJOR}.${MINOR}
 MAJOR_NAME=v${MAJOR}
 
-git push origin :"${MINOR_NAME}"
-git push origin :"${MAJOR_NAME}"
+echo "$MAJOR_NAME"
+echo "$MINOR_NAME"
+echo "$PATCH_NAME"
 
+git push origin ":$MINOR_NAME"
+git push origin ":$MAJOR_NAME"
+
+git tag -d "${MAJOR_NAME}"
 git tag -d "${MINOR_NAME}"
 git tag -d "${PATCH_NAME}"
 
-git tag "${MAJOR_NAME}"
-git tag "${MINOR_NAME}"
-git tag "${PATCH_NAME}"
+git tag -a "${MAJOR_NAME}" -m "Version $PATCH_NAME"
+git tag -a "${MINOR_NAME}" -m "Version $PATCH_NAME"
+git tag -a "${PATCH_NAME}" -m "Version $MAJOR_NAME"
 
 git push origin "${MAJOR_NAME}"
 git push origin "${MINOR_NAME}"
